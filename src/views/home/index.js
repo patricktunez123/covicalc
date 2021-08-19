5b842afb6c4aa158ae4a551925a280cbbbd38013
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2021-08-19 05:53:28 -0700

    integration

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ContactMe from "../../components/ContactMe";
 import Continents from "../../components/Continents";
 import Me from "../../components/Me";
 import { getCountries, getCountry } from "../../redux/actions/countryActions";
 import { getContinents } from "../../redux/actions/continentActions";
+import { numberWithCommas } from "../../helpers/numbersFormatter";
 import "./Home.scss";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const [selectedCountry, setSelectedCountry] = useState();
 
   const { loading: countriesLoading, data: countries } = useSelector(
     (state) => state.Countries
@@ -22,16 +24,23 @@ const Home = () => {
     (state) => state.Continents
   );
 
-  console.log("coutries data====>", countriesLoading, countries);
-  console.log("coutry data ****>>", countryLoading, country);
+  const handleChange = (e) => {
+    setSelectedCountry(e.target.value);
+  };
 
   useEffect(() => {
     dispatch(getCountries());
   }, [dispatch]);
 
+  let query = null;
+  if (selectedCountry === undefined) {
+    query = "Afghanistan";
+  } else {
+    query = selectedCountry;
+  }
   useEffect(() => {
-    dispatch(getCountry("rwanda"));
-  }, [dispatch]);
+    dispatch(getCountry(query));
+  }, [dispatch, query]);
 
   useEffect(() => {
     dispatch(getContinents());
@@ -43,10 +52,34 @@ const Home = () => {
         <div className="search--container">
           <h1 className="section-title">Updates</h1>
           <span>Search a country</span>
-          <div className="input">input goes here</div>
+          <div className="input">
+            <form>
+              {countriesLoading ? (
+                "loading"
+              ) : (
+                <select
+                  onChange={(e) => handleChange(e)}
+                  data-live-search="true"
+                >
+                  {countries?.map((country) => (
+                    <option
+                      key={country?.country}
+                      data-tokens={country?.country}
+                    >
+                      {country?.country}
+                    </option>
+                  ))}
+                </select>
+              )}
+            </form>
+          </div>
         </div>
         <div className="info__up--card">
-          <h1>{countryLoading ? "loading" : country?.cases}</h1>
+          <h1>
+            {countryLoading
+              ? "loading"
+              : country?.cases && numberWithCommas(country?.cases)}
+          </h1>
           <span>Cumulatively</span>
         </div>
       </div>
