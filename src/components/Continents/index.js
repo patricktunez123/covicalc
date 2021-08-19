3257ed20fd18a73568207a0cacbb4be45add7209
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2021-08-19 05:47:36 -0700

    ft-desin

--- src/components/Continents/index.js
+++ src/components/Continents/index.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./Continents.scss";
 
 const Continents = () => {
-  return <div className="covicalc--continents">continents</div>;
+  return (
+    <div className="covicalc--continents">
+      <div className="info__bottom">
+        <div className="info__bottom--card">
+          text tkajsdf aksdfj ak kajdf kaj fdaj dsfkj adksfj kajsdf ad sfkja
+          dfkj akdsjf kajsd fkja sdkfj aksdjf kajsd fkja ksdfj kajsdf kajs dkfj
+          aksdjf kajsdf kajs dkfja ksdfj kasdjf kjasdkfj ksdk
+        </div>
+      </div>
+      <div className="continents--container">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+      </div>
+    </div>
+  );
 };
 
 export default Continents;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="covicalc-footer">footer</div>;
+  return (
+    <div className="covicalc-footer">
+      <p>
+        Developed by{" "}
+        <a
+          href="https://tunez-patrick.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Patrick TUNEZERWANE
+        </a>
+      </p>
+      <p>
+        Designed by{" "}
+        <a href="https://awesomity.rw/" target="_blank" rel="noreferrer">
+          Awesomity Lab
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
